89106b2f88efbdcb20397d536c91047d91b6e372
Leonardo Collazo Klenina <kidbourbongamer@gmail.com>
2024-03-24 01:36:18 -0400

    Comments removed

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -42,14 +42,3 @@ function useLocalStorage(itemName, initialValue) {
 }
 
 export { useLocalStorage };
-
-// const defaultTodos = [
-//   { text: "Cortar cebolla", completed: true },
-//   { text: "Tomar el Curso de Intro a React.js", completed: true },
-//   { text: "Llorar con la Llorona", completed: false },
-//   { text: "LALALALALA", completed: false },
-//   { text: "Usar estados derivados", completed: true },
-// ];
-
-// localStorage.setItem("TODOS_V1", JSON.stringify(defaultTodos));
-// localStorage.removeItem("TODOS_V1");
